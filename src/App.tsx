a4595f2fd6b4f6343577fa42e5cdf5704e9a6ce8
Patryk Jesionka <jesionka.patryk13@gmail.com>
2024-09-07 04:27:47 +0100

    Added WIP redirect for portfolio-2024

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import ProjectDetailRelay from './pages/ProjectDetailRelay';
+import WIP from './pages/WIP';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/project/portfolio-2024" element={<WIP />} />
         <Route path="/project/:projectName" element={<ProjectDetailRelay />} />
       </Routes>
     </Router>
