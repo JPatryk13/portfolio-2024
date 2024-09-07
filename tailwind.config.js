2ea65f3883ced58d5e6034c964387aa268e84cb7
Patryk Jesionka <jesionka.patryk13@gmail.com>
2024-09-07 06:44:53 +0100

    Optimised the custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'teal-deer': {
-          DEFAULT: '#8FE1A1'
-        }
+        'teal-deer': '#8FE1A1'
       }
     },
   },
   plugins: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 }
 
