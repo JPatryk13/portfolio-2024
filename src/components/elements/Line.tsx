a3765693a6847523492b758d0fb31e61a5614b6b
Patryk Jesionka <jesionka.patryk13@gmail.com>
2024-09-07 06:43:39 +0100

    Made the lines more dynamic

--- src/components/elements/Line.tsx
+++ src/components/elements/Line.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const HorizontalLine: React.FC<{ className?: string }> = ({ className = "" }) => (
-    <hr className={`h-px border border-white border-opacity-60 ${className}`} />
+const HorizontalLine: React.FC<{ color?: string, opacity?: string, className?: string }> = ({ color = "white", opacity = "60", className = "" }) => (
+    // <hr className={`h-px border border-${color} border-opacity-60 ${className}`} />
+    <div className={`h-px border border-${color} opacity-${opacity} ${className}`} />
 );
-const VerticalLine: React.FC<{ className?: string }> = ({ className = "" }) => (
-    <hr className={`w-px border border-white border-opacity-60 ${className}`} />
+const VerticalLine: React.FC<{ color?: string, opacity?: string, className?: string }> = ({ color = "white", opacity = "60", className = "" }) => (
+    <hr className={`w-px border border-${color} opacity-${opacity} ${className}`} />
 );
 
 export {HorizontalLine, VerticalLine};
